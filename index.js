e5a3941fa5a6cf962ed8c99873908ef67e1cff42
creatrixity <creatrixity@gmail.com>
2018-10-25 11:10:39 +0100

    update package structure.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ace = require("@adonisjs/ace");
 ace.command(
-  "make:auth {name: Name of the user to greet}",
+  "make:auth",
   "Generates Views, Controllers and Models for user authentication",
   function({ name }) {
     console.log(`Scaffolding basic authentication`);
